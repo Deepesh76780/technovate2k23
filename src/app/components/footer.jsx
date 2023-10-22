168ed3eba3ffff4234a19f93e8a8f4c4d1ba720c
Deepesh76780 <da76780@gmail.com>
2023-10-22 05:06:35 -0700

    add footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,12 +1,19 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
+import { BsTwitter, BsFacebook, BsInstagram,BsLinkedin } from 'react-icons/bs'
 
 export default function Footer() {
   return (
-    <footer className="bg-black p-4 flex justify-center items-center">
-      <div className="flex space-x-4">
-        <a href="#" className="text-pink-400 hover:text-pink-600">Facebook</a>
-        <a href="#" className="text-pink-400 hover:text-pink-600">Twitter</a>
-        <a href="#" className="text-pink-400 hover:text-pink-600">Instagram</a>
+    <footer className="bg-black p-4 flex flex-col justify-center items-center gap-2">
+      <div className="flex space-x-4 ">
+        <Link href="#" className="text-slate-100 hover:text-pink-600"><BsTwitter /></Link>
+        <Link href="#" className="text-slate-100 hover:text-pink-600"><BsFacebook /></Link>
+        <Link href="#" className="text-slate-100 hover:text-pink-600"><BsInstagram /></Link>
+        <Link href="#" className="text-slate-100 hover:text-pink-600"><BsLinkedin /></Link>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-slate-100 text-xs">© 2023 Technovate - All Rights Reserved.</p>
       </div>
     </footer>
   )
