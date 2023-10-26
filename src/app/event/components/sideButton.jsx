16844a98e3a4fab8675ab668e40c606d967e4392
Deepesh76780 <da76780@gmail.com>
2023-10-26 01:18:28 -0700

    (fix:eventCard)

--- src/app/event/components/sideButton.jsx
+++ src/app/event/components/sideButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import {usePathname} from "next/navigation"
 
 
-const style="shadow-inner text-center font-Chakra shadow-pink-300 tracking-widest rounded-2xl p-3 uppercase w-32 hover:scale-110 transition-all duration-300"
+const style="shadow-inner card border-2  text-center font-Chakra  tracking-widest  p-3 uppercase w-32 hover:scale-110 transition-all duration-300"
 
 export default function LandingPage() {
 
@@ -11,10 +11,10 @@ export default function LandingPage() {
   console.log(pathname)
   return (
       <div className="flex flex-col w-[35%] gap-4 text-purple-300 items-center justify-center">
-            <Link className={`${style} ${pathname==="/event/pronight" ? "shadow-pink-500 text-pink-300":""}`} href={"/event/pronight"}>Pronight</Link>
-            <Link className={`${style} ${pathname==="/event/cultural" ? "shadow-pink-500 text-pink-300":""}`} href={"/event/cultural"}>Cultural</Link>
-            <Link className={`${style} ${pathname==="/event/informal" ? "shadow-pink-500 text-pink-300":""}`} href={"/event/informal"}>informal</Link>
-            <Link className={`${style} ${pathname==="/event/technical" ? "shadow-pink-500 text-pink-300":""}`} href={"/event/technical"}>technical</Link>
+            <Link className={`${style} ${pathname==="/event/pronight" ? "shadow-pink-400 border-pink-400 text-pink-300":"border-purple-400 shadow-pink-300"}`} href={"/event/pronight"}>Pronight</Link>
+            <Link className={`${style} ${pathname==="/event/cultural" ? "shadow-pink-400 border-pink-400 text-pink-300":"border-purple-400 shadow-pink-300"}`} href={"/event/cultural"}>Cultural</Link>
+            <Link className={`${style} ${pathname==="/event/informal" ? "shadow-pink-400 border-pink-400 text-pink-300":"border-purple-400 shadow-pink-300"}`} href={"/event/informal"}>informal</Link>
+            <Link className={`${style} ${pathname==="/event/technical" ? "shadow-pink-400 border-pink-400 text-pink-300":"border-purple-400 shadow-pink-300"}`} href={"/event/technical"}>technical</Link>
       </div>
   )
 }
--- src/app/pages/introVideo.jsx
+++ src/app/pages/introVideo.jsx
@@ -5,7 +5,7 @@ import BackgroundVideo from "../components/backgroundVideo";
 const IntroVideo = () => {
   return (
     <div className="relative w-screen h-screen  bg-[#00000071] shadow-2xl shadow-green-400">
-      <div className="rotate-12 border-2 border-pink-400">
+      <div className="rotate-12 border-2 border-pink-500 card">
         <BackgroundVideo src={"/background_videos/intro_video.mp4"} speed={1} />
       </div>
     </div>
