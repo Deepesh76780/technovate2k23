145b50e5988fe7dc8c25f05e185d0ad411979666
Deepesh76780 <da76780@gmail.com>
2023-11-07 06:51:43 -0800

    (style) changes

--- src/app/event/layout.jsx
+++ src/app/event/layout.jsx
@@ -24,7 +24,7 @@ export default function EventLayout(props) {
       />
       <Navbar bgcolor={navbarBgColor} />
       <main className="flex flex-col min-h-screen items-center justify-between overflow-x-hidden pt-16 px-16 p-5">
-        <div className="w-full justify-start relative z-1  skew-x-12 h-[60vh] my-auto">
+        <div className="w-full flex flex-row justify-start relative z-1  skew-x-12 h-[60vh] my-auto">
           <div className={`p-5  h-full w-full flex bg-[#00000080] border-2 border-purple-400 shadow-md shadow-purple-400 ${isOverflow} text-white w-[50%]`}>
             {props.children}
           </div>
