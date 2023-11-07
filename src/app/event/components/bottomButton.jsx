2f055ce82cbdcda633ac6f6dd6d6391b81efabbe
Deepesh76780 <da76780@gmail.com>
2023-11-07 06:56:02 -0800

    (style) changes

--- src/app/event/components/bottomButton.jsx
+++ src/app/event/components/bottomButton.jsx
@@ -32,7 +32,7 @@ export default function SideButton() {
       variants={item}
       initial="hidden"
       animate="show"
-      className="flex w-[40%] gap-4 text-purple-300 items-center justify-center">
+      className="flex w-[40%] gap-4 text-purple-300 items-center justify-center place-self-center">
       <Link className={`${style} ${pathname.startsWith("/event/pronight") ? "shadow-pink-400 border-pink-400 text-pink-300" : "border-purple-400 shadow-pink-300"}`} href={"/event/pronight"}>Pronight</Link>
       <Link className={`${style} ${pathname.startsWith("/event/cultural") ? "shadow-pink-400 border-pink-400 text-pink-300" : "border-purple-400 shadow-pink-300"}`} href={"/event/cultural"}>Cultural</Link>
       <Link className={`${style} ${pathname.startsWith("/event/informal") ? "shadow-pink-400 border-pink-400 text-pink-300" : "border-purple-400 shadow-pink-300"}`} href={"/event/informal"}>informal</Link>
--- src/app/event/layout.jsx
+++ src/app/event/layout.jsx
@@ -23,9 +23,9 @@ export default function EventLayout(props) {
         priority={true}
       />
       <Navbar bgcolor={navbarBgColor} />
-      <main className="flex flex-col min-h-screen items-center justify-between overflow-x-hidden pt-16 px-16 p-5">
-        <div className="w-full flex flex-row justify-start relative z-1  skew-x-12 h-[60vh] my-auto">
-          <div className={`p-5  h-full w-full flex bg-[#00000080] border-2 border-purple-400 shadow-md shadow-purple-400 ${isOverflow} text-white w-[50%]`}>
+      <main className="flex flex-col min-h-screen  overflow-x-hidden pt-16 px-16 p-5">
+        <div className="w-[50%] flex flex-row justify-start relative z-1  skew-x-12 h-[60vh] my-auto">
+          <div className={`p-5  h-full w-full flex bg-[#00000080] border-2 border-purple-400 shadow-md shadow-purple-400 ${isOverflow} text-white`}>
             {props.children}
           </div>
         </div>
