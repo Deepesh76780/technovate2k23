ba5f726014f6d5b57da3b4d3914a575a86e3ac2e
Deepesh76780 <da76780@gmail.com>
2023-12-20 19:20:52 +0530

    Update logo image and add countdown timer to landing page

--- src/app/pages/landingPageContent.jsx
+++ src/app/pages/landingPageContent.jsx
@@ -23,8 +23,23 @@ const containerVariants = {
 
 const LandingPageContent = () => {
   const [scrollY, setScrollY] = useState(0);
+  const [time, setTime] = useState();
   const [hover, setHover] = useState(false);
   const scale = (1 + scrollY / 500) > 6 ? 6 : (1 + scrollY / 500);
+  var myfunc = setInterval(function () {
+    var countDownDate = new Date("Feb 16, 2024 00:00:00").getTime();
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    setTime(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+    if (timeleft < 0) {
+      clearInterval(myfunc)
+      setTime("Arrived at Technovate")
+    }
+  }, 1000)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -42,7 +57,7 @@ const LandingPageContent = () => {
     <div className={`absolute top-0 left-0 w-full h-full ${hover ? "bg-black" : "bg-opacity-50"} transition-all duration-500 z-10 overlay`}>
       <Navbar bgcolor={"bg-[#e960a5a8]"} resColor={"#e960a5a8"} />
       <motion.div
-        className="w-full lg:h-[80%]  h-[83%] flex overflow-x-hidden"
+        className="w-full lg:h-[73%]  h-[83%] flex overflow-x-hidden"
         variants={containerVariants}
         initial="hidden"
         animate="visible"
@@ -79,6 +94,14 @@ const LandingPageContent = () => {
         onMouseLeave={() => setHover(false)}
         className="pink_selector h-fit  border-2  hover:border-pink-500 hover:bg-black hover:rounded-xl hover:scale-125 hover:backdrop-hue-rotate-30 hover:text-pink-400 transition-all duration-500 cursor-pointer tracking-[0.3em] flex place-self-center text-center  border-slate-200 lg:text-2xl  text-xl  rounded-2xl font-cyberway  text-slate-50 lg:p-4 p-2 mx-auto w-fit backdrop-hue-rotate-90">
         REGISTER
+      </motion.div>
+      <motion.div
+        className="pink_selector  text-white mx-auto w-fit tracking-[0.15em] mt-2  font-Chakra lg:text-lg text-sm  p-2"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2, duration: 1 }}
+      >
+        Arriving at {time}
       </motion.div>
     </div >
   );
