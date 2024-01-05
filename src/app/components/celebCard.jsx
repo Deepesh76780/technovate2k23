b89b7174e31eaeddd36a608b4a50b61ccac41540
shivamk-21 <2103shivamk@gmail.com>
2024-01-05 23:58:22 +0530

    Updated past Performances Images

--- src/app/components/celebCard.jsx
+++ src/app/components/celebCard.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function EventCard({ Img }) {
   return (
-    <Card className="bg-[#14101084] styP  flex shadow-inner shadow-pink border-2 border-pink-500  text-slate-100 md:w-80 w-64 lg:h-[22rem] h-[20rem]  mx-12">
+    <Card className="bg-[#14101084] styP  flex shadow-inner shadow-pink border-2 border-pink-500  text-slate-100 md:w-auto w-64 lg:h-[22rem] h-[20rem]  mx-12">
       <Image
         src={Img}
         width={1000}
--- src/app/pages/aboutEvent.jsx
+++ src/app/pages/aboutEvent.jsx
@@ -1,47 +1,59 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import Marquee from "react-fast-marquee";
 import EventCard from "../components/celebCard";
-import Title from '../components/Title';
+import Title from "../components/Title";
 
-
-
-const imgData = [{
-  href: "/past_star_images/ash_king.png",
-},
-{
-  href: "/past_star_images/aashish_solanki.webp",
-}
-  ,
-{
-  href: "/past_star_images/gajendra_verma.jpg",
-}
-  ,
-{
-  href: "/past_star_images/carnival.png",
-}
-]
+const imgData = [
+  {
+    href: "/past_star_images/1.jpg",
+  },
+  {
+    href: "/past_star_images/2.JPG",
+  },
+  {
+    href: "/past_star_images/3.JPG",
+  },
+  {
+    href: "/past_star_images/4.JPG",
+  },
+  {
+    href: "/past_star_images/5.JPG",
+  },
+  {
+    href: "/past_star_images/6.JPG",
+  },
+  {
+    href: "/past_star_images/7.JPG",
+  },
+  {
+    href: "/past_star_images/8.JPG",
+  },
+  {
+    href: "/past_star_images/9.JPG",
+  },
+  {
+    href: "/past_star_images/gajendra_verma.jpg",
+  },
+  {
+    href: "/past_star_images/11.JPG",
+  },
+];
 
 export default function AboutEvent() {
-
   return (
     <div className="relative overflow-hidden w-full">
       <section className="lg:pt-32 lg:pb-32 pb-20 pt-20 relative">
-        <div
-          className="absolute w-full h-full top-0 left-0 bg-event-parallex bg-cover bg-center bg-no-repeat opacity-90  bg-fixed"
-
-        ></div>
+        <div className="absolute w-full h-full top-0 left-0 bg-event-parallex bg-cover bg-center bg-no-repeat opacity-90  bg-fixed"></div>
         <div className="w-full flex flex-col lg:gap-28 gap-14">
-          <Title title={"PAST PERFORMERS"} />
-          <Marquee direction='left' pauseOnClick={true}>
-            {
-              imgData.map((item, index) => {
-                return <EventCard Img={item.href} key={index} />
-              })
-            }
+          <Title title={"PAST PERFORMANCES"} />
+          <Marquee direction="left" pauseOnClick={true}>
+            {imgData.map((item, index) => {
+              return <EventCard Img={item.href} key={index} />;
+            })}
           </Marquee>
         </div>
-      </section >
-    </div >
-  )
+      </section>
+    </div>
+  );
 }
