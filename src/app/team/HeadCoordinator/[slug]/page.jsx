772172cd5090275d7624256a3f32551c7d7632bf
Deepesh76780 <da76780@gmail.com>
2024-01-07 20:18:14 +0530

    few changes

--- src/app/team/HeadCoordinator/[slug]/page.jsx
+++ src/app/team/HeadCoordinator/[slug]/page.jsx
@@ -1,9 +1,7 @@
 import PlayerInfo from "../../components/playerInformation"
 import { team } from "../../../../styles/data"
-import Specail from "../../components/Specail"
+
 export default function page({ params }) {
-
-
   const data = team.find(item => item.id === +params.slug)
 
   return (
