a507debbd758382602635b290d495d61ce329d3b
shivamk-21 <2103shivamk@gmail.com>
2024-01-07 18:33:04 +0530

    Modified Data and Fixed Issues

--- src/app/team/organizingCommittee/[slug]/page.jsx
+++ src/app/team/organizingCommittee/[slug]/page.jsx
@@ -1,10 +1,10 @@
 import PlayerInfo from "../../components/playerInformation"
-import { team } from "../../../../styles/data"
+import { organizingCommite } from "../../../../styles/data"
 
 export default function page({ params }) {
 
 
-  const data = team.find(item => item.id === +params.slug)
+  const data = organizingCommite.find(item => item.id === +params.slug)
 
   return (
     <PlayerInfo props={data} />
