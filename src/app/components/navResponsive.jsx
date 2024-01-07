f76215e2a969b43f0a54ec8f3dea5ee5eecb27f4
Deepesh76780 <da76780@gmail.com>
2024-01-07 20:15:32 +0530

    Refactor responsive navigation component

--- src/app/components/navResponsive.jsx
+++ src/app/components/navResponsive.jsx
@@ -1,31 +1,12 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { NavButton } from "./navButton";
 import { useState } from "react";
-import Audio from "./backgroundAudio";
 
 const NavResponsive = ({ navData, bgColor }) => {
   const [isOpen, setOpen] = useState(false);
-
-  const responsive = {
-    hidden: {
-      opacity: 0,
-      x: "-100vw",
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 1, ease: "easeInOut" },
-    },
-    exit: {
-      x: "-100vw",
-      opacity: 0,
-      transition: { duration: 1, ease: "easeInOut" },
-    },
-  };
 
   return (
     <div className="w-full lg:hidden visible ">
@@ -49,14 +30,9 @@ const NavResponsive = ({ navData, bgColor }) => {
           </svg>
         </>
       </div>
-      <AnimatePresence>
         {isOpen ? (
-          <motion.div
-            variants={responsive}
-            key={"modal"}
-            initial={"hidden"}
-            animate={"visible"}
-            exit={"exit"}
+          <div
+           
             className={`z-10 flex flex-col border-2  border-pink-700 h-[100dvh] fixed  top-0 gap-5 place-items-center justify-around w-full bg-[#0000009a]`}
           >
             <div className="flex flex-col gap-10 items-center">
@@ -91,11 +67,10 @@ const NavResponsive = ({ navData, bgColor }) => {
                 </button>
               </a>
             </div>
-          </motion.div>
+          </div>
         ) : (
           <></>
         )}
-      </AnimatePresence>
     </div>
   );
 };
