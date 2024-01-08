0b64f1ff4e60796c92d64723c2659c7aaf18fc5d
Deepesh76780 <da76780@gmail.com>
2024-01-08 09:37:34 +0530

    register button fix

--- src/app/pages/landingPageContent.jsx
+++ src/app/pages/landingPageContent.jsx
@@ -95,23 +95,18 @@ const LandingPageContent = () => {
           </div>
         </div>
       </div>
-
-      <motion.div
+      <motion.a
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 2 }}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
+        href="https://unstop.com/college-fests/technovate-2024-international-institute-of-information-technology-iiit-naya-raipur-183351/amp"
+        target="_blank"
         className="pink_selector h-fit  border-2   hover:border-pink-500 bg-[#000000a1] hover:bg-black hover:rounded-xl hover:scale-125 hover:backdrop-hue-rotate-30 hover:text-pink-400 transition-all duration-500 cursor-pointer tracking-[0.3em] flex place-self-center text-center  border-slate-200 lg:text-2xl  text-xl  rounded-2xl font-cyberway  text-slate-50 lg:p-4 p-2 mx-auto w-fit "
       >
-        <a
-          href="https://unstop.com/college-fests/technovate-2024-international-institute-of-information-technology-iiit-naya-raipur-183351/amp"
-          target="_blank"
-        >
-          <p>REGISTER</p>
-        </a>
-      </motion.div>
-
+        <p>REGISTER</p>
+      </motion.a>
       <motion.div
         className="pink_selector   mx-auto w-fit tracking-[0.15em] mt-3  font-Michroma font-semibold  lg:text-lg text-sm  p-2   text-slate-50 rounded-full"
         initial={{ opacity: 0 }}
