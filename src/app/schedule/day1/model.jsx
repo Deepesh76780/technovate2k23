5a356eebb0606b22c623ea7a51c6a8c4286d6394
Deepesh76780 <da76780@gmail.com>
2024-01-16 12:04:21 +0530

    few changes in model

--- src/app/schedule/day1/model.jsx
+++ src/app/schedule/day1/model.jsx
@@ -11,11 +11,9 @@ import {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("/draco-gltf/");
     const model = useGLTF("/map.glb", dracoLoader);
-    const controlsRef = useRef();
     const [zoom, setZoom] = useState(1);
 
     const handleWheel = (event) => {
-      // Adjust the zoom factor based on the wheel delta
       const newZoom = Math.max(0.1, Math.min(2, zoom + event.deltaY * 0.001));
       setZoom(newZoom);
     };
@@ -24,11 +22,7 @@ import {
       <>
         <Environment preset="warehouse" />
           <PresentationControls
-          ref={controlsRef}
           global={{ polar: [-0.4, 0.2], azimuth: [-0.4, 0.2] }}
-          zoomSpeed={0.5} 
-          minZoom={0.5}    
-          maxZoom={2.0}    
         >
           <primitive object={model.scene} position-y={-2}  scale={[6 * zoom, 6 * zoom, 6 * zoom]} onWheel={handleWheel}></primitive>
         </PresentationControls>
--- src/app/schedule/day1/page.jsx
+++ src/app/schedule/day1/page.jsx
@@ -1,26 +1,28 @@
 "use client";
 import { Suspense } from "react";
-import { Html, useProgress } from '@react-three/drei'
+import { Html, useProgress } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import Model from "./model";
 
-
-
 function Loader() {
-  const { progress } = useProgress()
-  return <Html center>{progress.toFixed()}%</Html>
+  const { progress } = useProgress();
+  return <Html center>{progress.toFixed()}%</Html>;
 }
-
 
 const Home = () => {
   return (
-      <Canvas>
-        <Suspense fallback={<Loader />}>
-          <Model/>
-          <Environment preset="sunset"  />
-        </Suspense>
-      </Canvas>
+    <div className="flex h-full w-full items-center">
+      <div className="h-full w-3/6 ">
+        <Canvas>
+          <Suspense fallback={<Loader />}>
+            <Model />
+            <Environment preset="sunset" />
+          </Suspense>
+        </Canvas>
+      </div>
+      <div></div>
+    </div>
   );
 };
 
