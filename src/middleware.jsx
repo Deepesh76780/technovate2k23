d5d5d29e6603977f4c0d2dbe71f1edf7aa049610
Deepesh76780 <da76780@gmail.com>
2024-01-19 17:51:58 +0530

    Fix incorrect pathname in middleware.jsx

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
 const url = request.nextUrl.clone()
 url.pathname = '/'
-  if (request.nextUrl.pathname==='/event') {
+  if (request.nextUrl.pathname==='/events') {
     return NextResponse.redirect(url)
   }
     return NextResponse.next()
