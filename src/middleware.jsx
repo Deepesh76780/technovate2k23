bce2500d7c109d641f88d706a9f0b5530a811c4a
shivamk-21 <2103shivamk@gmail.com>
2024-01-20 10:18:09 +0530

    Some Metadata for middleware

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,11 +1,35 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
- 
+export async function generateMetadata({ params }) {
+  return {
+    title: "Technovate 2024",
+    description: `Technovate at IIIT-NR is an annual convergence of technology and culture,
+       encompassing coding competitions, hacking contests, battle of bands, 
+       and keynote sessions by innovation experts. Spanning three dynamic days, 
+       it provides a platform for inventive thinking, spotlighting ingenious ideas 
+       and cultural talents. Attendees can engage in collaborative learning, network 
+       with like-minded individuals, and explore cutting-edge technologies.The event
+        includes stand-up performances, soul-stirring melodies, and EDM beats, creating 
+        a vibrant oasis where students can celebrate their skills and passions. Following 
+        the success of the previous edition with 5000 participants, this year anticipates 
+        an even larger turnout of over 7000, solidifying Technovate as a thriving celebration 
+        of creativity and expertise. With its dynamic mix of events and a growing community, 
+        Technovate continues to evolve as a significant platform where technology and culture converge harmoniously.`,
+    openGraph: {
+      image: [
+        {
+          url: "/logo/logo.png",
+        },
+      ],
+    },
+  };
+}
+
 export function middleware(request) {
-const url = request.nextUrl.clone()
-url.pathname = '/'
-  if (request.nextUrl.pathname==='/event') {
-    return NextResponse.redirect(url)
+  const url = request.nextUrl.clone();
+  url.pathname = "/";
+  if (request.nextUrl.pathname === "/event") {
+    return NextResponse.redirect(url);
   }
-    return NextResponse.next()
+  return NextResponse.next();
 }
